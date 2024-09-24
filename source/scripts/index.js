5e4d5e84cb9ef5b2b8b3505545140944d41ec7e0
Lisa Chudesnova <154272777+CalmOwl@users.noreply.github.com>
2024-09-24 02:34:25 +0200

    Добавляет адаптивную вёрстку (#13)
    * Добавляет адаптивную вёрстку
    
    * Исправляет замечания линтера

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,15 @@
 /* в этот файл добавляет скрипты*/
+const navMain = document.querySelector('.main-header');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-header--nojs');
+
+navToggle.addEventListener('click', () => {
+  if (navMain.classList.contains('main-header--closed')) {
+    navMain.classList.remove('main-header--closed');
+    navMain.classList.add('main-header--opened');
+  } else {
+    navMain.classList.add('main-header--closed');
+    navMain.classList.remove('main-header--opened');
+  }
+});
